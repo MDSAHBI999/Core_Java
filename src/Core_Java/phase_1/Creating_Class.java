ad9b384919e1b23c9f0578bb1b14651aa7f6af03
your-username <your-email@example.com>
2025-02-07 23:45:16 +0530

    Initial commit

---
+++ src/Core_Java/phase_1/Creating_Class.java
@@ -0,0 +1,33 @@
+package Core_Java.phase_1;
+
+public class Creating_Class {
+
+
+    public static String greet(String name) {
+        return "Hey it's , " + name + "Here";
+    }
+
+    int mult(int a ,int b ){
+        return a*b;
+    }
+    float add(int a ,int b){
+        return a + b ;
+    }
+    public static void main(String[] args){
+
+        String greeting = greet("MD ");
+        System.out.println(greeting);
+
+        // Creating object for mult (multiplication)  in class Creating_Class .
+        Creating_Class obj1 = new Creating_Class();
+        // printing the multiplication without any third variable or datatype .
+        System.out.println("multipication is " + obj1.mult(112 ,31  ) );
+
+        // Creating object for add (addition)  in class Creating_Class .
+        Creating_Class obj2 = new Creating_Class();
+        // Storing output in third variable .
+        float result = obj2.add(1021, 212);
+        // printing result of addition .
+        System.out.println(" addition is "+ result);
+    }
+}
