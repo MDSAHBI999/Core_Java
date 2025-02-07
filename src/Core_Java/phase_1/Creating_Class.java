71b0e55f585ae964deedd4200f09983abc02b9e3
your-username <your-email@example.com>
2025-02-08 00:03:16 +0530

    mult method is added.

--- src/Core_Java/phase_1/Creating_Class.java
+++ src/Core_Java/phase_1/Creating_Class.java
@@ -6,6 +6,11 @@ public class Creating_Class {
     public static String greet(String name) {
         return "Hey it's , " + name + "Here";
     }
+
+    float mult(int a ,int b){
+        return a * b ;
+    }
+
     float add(int a ,int b){
         return a + b ;
     }
@@ -17,7 +22,7 @@ public class Creating_Class {
         // Creating object for mult (multiplication)  in class Creating_Class .
         Creating_Class obj1 = new Creating_Class();
         // printing the multiplication without any third variable or datatype .
-        System.out.println("multipication is " + obj1.mult(112 ,31  ) );
+        System.out.println("multipication is " + obj1.mult(11 ,3) );
 
         // Creating object for add (addition)  in class Creating_Class .
         Creating_Class obj2 = new Creating_Class();
