a1334c538657d1114b36e03740a0991981039e24
your-username <your-email@example.com>
2025-02-08 22:48:48 +0530

    A Java program to find the range of int using the Integer wrapper class.

---
+++ src/Core_Java/phase_1/range_of_int.java
@@ -0,0 +1,12 @@
+package Core_Java.phase_1;
+
+public class range_of_int {
+    public static void main (String[] agrs){
+        System.out.println(
+                "minimum range :"+Integer.MIN_VALUE
+        );
+        System.out.println(
+                "minimum range :"+Integer.MAX_VALUE
+        );
+    }
+}
