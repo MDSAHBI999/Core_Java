3651017f123791f4116b3c9435c48bef3da0482f
your-username <your-email@example.com>
2025-02-08 22:49:14 +0530

    A Java program to find the range of long using the Long wrapper class.

---
+++ src/Core_Java/phase_1/length_of_long.java
@@ -0,0 +1,13 @@
+package Core_Java.phase_1;
+
+public class length_of_long {
+    public static void  main(String[] args){
+        System.out.println(
+                "length of Long :"+Long.MIN_VALUE
+        );
+        System.out.println(
+                "length of Long :"+Long.MAX_VALUE
+        );
+    }
+}
+
