3a9bb91b37a0e08c69ed2c4ad14f56f68e1b359d
your-username <your-email@example.com>
2025-02-09 17:48:25 +0530

    A Java program to find the range of long using the Long wrapper class.

---
+++ src/Core_Java/phase_1/Practice_Array.java
@@ -0,0 +1,9 @@
+package Core_Java.phase_1;
+import java.util.Arrays;
+
+public class Practice_Array {
+    public static void main (String[] args){
+        String country = "India"
+                int pop =
+    }
+}
