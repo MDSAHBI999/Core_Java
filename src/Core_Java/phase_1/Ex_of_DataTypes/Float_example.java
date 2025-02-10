01b8b2d33cc04298e8549a73df0e1bd82e028747
your-username <your-email@example.com>
2025-02-10 22:31:32 +0530

    created a float example for practice .

---
+++ src/Core_Java/phase_1/Ex_of_DataTypes/Float_example.java
@@ -0,0 +1,23 @@
+package Core_Java.phase_1.Ex_of_DataTypes;
+
+ class floate_range {
+    public static void range_of_floate() {
+        System.out.println("The Lowest Range : "+ Float.MIN_VALUE);
+        System.out.println("The Highest Range : "+ Float.MAX_VALUE);
+    }
+    public static void main(String [] args ){
+        range_of_floate();
+        System.out.println(0.3 + 0.2 );
+        float b = 1.5f;
+        float temp = 29.6f ;
+        float acc_bal = 100000000000f;
+        float moduler_ASCII_Val = '%'; // we acan store the ACSII valuer of any character  in any datatype except boolean
+
+        System.out.println(b);
+        System.out.println(temp);
+        System.out.println(acc_bal);
+        System.out.println(moduler_ASCII_Val);
+
+    }
+}
+
