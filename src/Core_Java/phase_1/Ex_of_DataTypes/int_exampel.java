7bcb70ff0e69060d6b9a344319d26570517ba963
your-username <your-email@example.com>
2025-02-11 15:02:58 +0530

    created a float example for practice .

---
+++ src/Core_Java/phase_1/Ex_of_DataTypes/int_exampel.java
@@ -0,0 +1,21 @@
+package Core_Java.phase_1.Ex_of_DataTypes;
+
+public class int_exampel {
+    public static void main(String[] agrs) {
+        System.out.println("minimum range :-2147483648");
+        System.out.println("minimum range :2147483647");
+        int_exampel.exmaple_int.int_exaompel();
+    }
+    static class exmaple_int {
+        exmaple_int() {
+        }
+        public static void int_exaompel() {
+            int city_Population = 872342838;
+            int pin_code = 413305;
+            int earth_to_moon = 384400;
+            System.out.println("The city population :" + city_Population);
+            System.out.println(" the pin code of city :" + pin_code);
+            System.out.println(" The distance of moon from earth :" + earth_to_moon);
+        }
+    }
+}
