959a5f93a68fbff1831266da824a062e83244fc4
your-username <your-email@example.com>
2025-02-15 08:43:36 +0530

    new program  temp conversion from C to F is added in package Programing_Lectures_Codes.

---
+++ src/Core_Java/phase_1/Programing_Lectures_Codes/Temp_CtoF.java
@@ -0,0 +1,11 @@
+package Core_Java.phase_1.Programing_Lectures_Codes;
+
+public class Temp_CtoF {
+    public static void main(String[] args) {
+        var TempIncelsius = 32.7;
+        double Temp_In_fahrenheit = (TempIncelsius * (9/5)+32);
+        System.out.println(" \n Temperature in fahrenheit :"+Temp_In_fahrenheit);
+
+    }
+
+}
