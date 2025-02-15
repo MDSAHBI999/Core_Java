44edc71cac2af27c718ac355b487a448bbd13937
your-username <your-email@example.com>
2025-02-15 08:40:25 +0530

    new program  area of circle is added in package  Programing_Lectures_Codes.

---
+++ src/Core_Java/phase_1/Programing_Lectures_Codes/AreaOfCircle.java
@@ -0,0 +1,11 @@
+package Core_Java.phase_1.Programing_Lectures_Codes;
+
+public class AreaOfCircle {
+    public static void main (String[] args ){
+        double pi = (double) (22/7);
+        var r = 7;
+        double area = pi * r*r ;
+        System.out.println("\n the Area of Cirecle :"+area);
+
+    }
+}
--- src/Core_Java/phase_1/Programing_Lectures_Codes/codeoi.java
+++
@@ -1,7 +0,0 @@
-package Core_Java.phase_1.Programing_Lectures_Codes;
-
-public class codeoi {
-    public static void main (String[] args ){
-        System.out.println("ninja killer ");
-    }
-}
