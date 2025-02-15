9c01498b61f265a3444d42160a9757408384d6ad
your-username <your-email@example.com>
2025-02-15 08:42:47 +0530

    new program  perimeter of rectangle  is added in package Programing_Lectures_Codes.

---
+++ src/Core_Java/phase_1/Programing_Lectures_Codes/PerimeterOfRectangle.java
@@ -0,0 +1,12 @@
+package Core_Java.phase_1.Programing_Lectures_Codes;
+
+public class PerimeterOfRectangle {
+    public static void main(String[] args) {
+        var a = 34;
+        var b = 45;
+        var permeter = 2*(a+b);
+        System.out.println("\n");
+        System.out.println("The perimeter of rectangle of "+ permeter);
+
+    }
+}
