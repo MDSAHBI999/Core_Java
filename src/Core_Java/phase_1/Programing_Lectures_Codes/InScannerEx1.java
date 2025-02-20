52a9d6e04be4a8d7844ed30a62518d0e7c299056
your-username <your-email@example.com>
2025-02-20 09:54:40 +0530

    In this we have added Two  new class named as  InScannerEX2 and InScannerEX1 inisde the  package Programing_Lectures_Codes .

---
+++ src/Core_Java/phase_1/Programing_Lectures_Codes/InScannerEx1.java
@@ -0,0 +1,29 @@
+package Core_Java.phase_1.Programing_Lectures_Codes;
+
+import java.util.Scanner;
+
+public class InScannerEx1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the below details :" );
+
+        System.out.println("enter the name:");
+        String name = sc.next();
+
+        System.out.println("enter the phone number :");
+        Long  Phone_no  = sc.nextLong();
+
+        System.out.println("enter the weight  :");
+        float weight    = sc.nextFloat();
+
+        System.out.println("enter the Height  :");
+        float height = sc.nextFloat();
+
+        System.out.println("enter the Email_Id  :");
+        String Email_id = sc.next();
+
+
+        System.out.println(name +"\n"+ Phone_no +"\n"+ weight+"\n"+height+"\n"+Email_id );
+
+    }
+}
